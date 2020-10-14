6a649cb8e66d22b2e42be9988a8caf7b8b26499a
khizerkhan-2316 <60915859+khizerkhan-2316@users.noreply.github.com>
2020-10-14 19:57:32 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ console.log("Up and running");
 
 // Nedtående er javascript til "Portfolipage.html":
 
+//for mobile and ipad
 document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects")}
 document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects")}
 
+//for pc-laptos, computers: 
 document.getElementById("mmd").onclick = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects")}
 document.getElementById("dm").onclick = function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects")}
 
-
+//functions:
 function showElement(element){
 document.getElementById(element).style.display = "block";
 }
