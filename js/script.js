786d1e974062ec9b80381c85105d10d6b4b557ac
khizerkhan-2316 <60915859+khizerkhan-2316@users.noreply.github.com>
2020-10-21 00:06:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ console.log("Up and running");
 // Nedtående er javascript til "Portfolipage.html":
 
 //for mobile and ipad
-document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects")}
-document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects")}
+document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindow()}
+document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), scrollWindow()}
 
 //for pc-laptos, computers: 
-document.getElementById("mmd").onclick = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects")}
-document.getElementById("dm").onclick = function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects")}
+document.getElementById("mmd").onclick = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindow()}
+document.getElementById("dm").onclick = function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), scrollWindow()}
 
 //functions:
 function showElement(element){
@@ -26,4 +26,8 @@ function showboxElements(element){
 document.getElementById(element).style.display = "flex";   
 }
 
+
+function scrollWindow(){
+window.scrollTo(0, window.innerHeight /1.1);
+}
 // Javascript slut for "portfolie.html"!
