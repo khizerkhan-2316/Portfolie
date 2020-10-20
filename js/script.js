78938458ee6502c433f0c8e8d08467658889e668
khizerkhan-2316 <60915859+khizerkhan-2316@users.noreply.github.com>
2020-10-21 00:26:20 +0200

    update

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ console.log("Up and running");
 // Nedtående er javascript til "Portfolipage.html":
 
 //for mobile and ipad
-document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindow()}
-document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), scrollWindow()}
+document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindowMobile()}
+document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), scrollWindowMobile()}
 
 //for pc-laptos, computers: 
 document.getElementById("mmd").onclick = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindow()}
@@ -29,5 +29,10 @@ document.getElementById(element).style.display = "flex";
 
 function scrollWindow(){
 window.scrollTo(0, window.innerHeight /1.1);
+}
+
+function scrollWindowMobile(){
+    window.scrollTo(0, window.innerHeight /1.05);
+
 }
 // Javascript slut for "portfolie.html"!
