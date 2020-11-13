08fa86802eff2f11896491910c5579d3a35a402a
Khizer Khan <khizer759@gmail.com>
2020-11-13 21:17:20 +0100

    added gif to display my portfolios

--- js/script.js
+++ js/script.js
@@ -1,36 +1,71 @@
 console.log("Up and running");
 
-
 // Nedtående er javascript til "Portfolipage.html":
 
 //for mobile and ipad
-document.getElementById("mmd").ontouchstart = function(){showElement("headingMmd"), hideElement("headingDm"), showboxElements("mmdProjects"), scrollWindowMobile()}
-document.getElementById("dm").ontouchstart= function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), scrollWindowMobile()}
+document.getElementById("mmd").ontouchstart = function () {
+  showElement("headingMmd"),
+    hideElement("headingDm"),
+    showboxElements("mmdProjects"),
+    scrollWindowMobile();
+};
+document.getElementById("dm").ontouchstart = function () {
+  showElement("headingDm"),
+    hideElement("headingMmd"),
+    hideElement("mmdProjects"),
+    scrollWindowMobile();
+};
 
-//for pc-laptos, computers: 
-document.getElementById("mmd").onclick = function(){showElement("headingMmd"), hideElement("headingDm"), hideElement("datamatikerProjects"), showboxElements("mmdProjects"), scrollWindow()}
-document.getElementById("dm").onclick = function(){showElement("headingDm"), hideElement("headingMmd"), hideElement("mmdProjects"), showboxElements('datamatikerProjects'), scrollWindow()};
+//for pc-laptos, computers:
+document.getElementById("mmd").onclick = function () {
+  showElement("headingMmd"),
+    hideElement("headingDm"),
+    hideElement("datamatikerProjects"),
+    showboxElements("mmdProjects"),
+    scrollWindow();
+};
+document.getElementById("dm").onclick = function () {
+  showElement("headingDm"),
+    hideElement("headingMmd"),
+    hideElement("mmdProjects"),
+    showboxElements("datamatikerProjects"),
+    scrollWindow();
+};
 
 //functions:
-function showElement(element){
-document.getElementById(element).style.display = "block";
+function showElement(element) {
+  document.getElementById(element).style.display = "block";
 }
 
-
-function hideElement(element){
-document.getElementById(element).style.display = "none";
-
+function hideElement(element) {
+  document.getElementById(element).style.display = "none";
 }
 
-function showboxElements(element){
-document.getElementById(element).style.display = "flex";   
+function showboxElements(element) {
+  document.getElementById(element).style.display = "flex";
 }
 
-
-function scrollWindow(){
-window.scrollTo(0, window.innerHeight /1);
+function scrollWindow() {
+  window.scrollTo(0, window.innerHeight / 1);
 }
 
+const hover = () => {
+  hideElement("zalando-img");
+  showElement("zalando-gif");
+};
+
+const hoverOut = () => {
+  hideElement("zalando-gif");
+  showElement("zalando-img");
+};
+
+document.getElementById("zalando-img").onmouseover = function () {
+  hover();
+};
+
+document.getElementById("zalando-gif").onmouseout = function () {
+  hoverOut();
+};
 /*
 function scrollWindowMobile(){
     window.scrollTo(0, window.innerHeight /1);
